165dd98e195a58a266fc8dc465c8f5d133391de4
tr1fker <cfjhxhkxck@gmail.com>
2025-08-20 16:04:45 +0300

    Implement CLI guess-number combining InputHandler and GeneratorHandler

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,5 +1,17 @@
 public class GuessNumber {
     public static void main(String[] args) {
+        GeneratorHandler generatorHandler = new GeneratorHandler();
+        InputHandler inputHandler = new InputHandler();
+
+        System.out.print("Введите число от 1 до 100:");
+        int number = inputHandler.inputInteger();
+
+        int randomNumber = generatorHandler.getRandomNumber();
+
+        if (randomNumber == number)
+            System.out.print("Вы угадали!!!");
+        else
+            System.out.print("Увы... Было загадано число " + randomNumber);
 
     }
 }
